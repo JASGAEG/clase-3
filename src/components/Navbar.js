6cf7c05571f8fa99df9e66aa996cc17161ae1dc1
Javier <chef.javier.sosa@gmail.com>
2022-03-28 11:28:06 -0600

    Modificación de estilos para el navbar de react

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div className="App">
+            <div className="izquierda">Sosssa</div>
+            <ul className="menu">
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="index.html"> Tienda</a></li>
+                <li><a href="index.html"> Subastas</a></li>
+                <li><a href="index.html"> Contacto</a></li> 
+            </ul>           
+        </div>
+    );
+}
+
+export default Navbar;
